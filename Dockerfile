976a76f356fdffa9425092976fbdb33d48781696
Karan Botadra <kbot983@gmail.com>
2020-04-18 01:52:13 -0400

    Trying new things that didnt went out so well.

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM python
-ADD app.py /
-COPY requirements.txt ./
-RUN pip install --no-cache-dir -r requirements.txt
-CMD ["python", "./app.py"]
--- init-sql/setup.sql
+++ init-sql/setup.sql
@@ -1,16 +1,50 @@
+-- Adminer 4.7.6 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+SET NAMES utf8mb4;
 CREATE DATABASE scraping;
 USE scraping;
+DROP TABLE IF EXISTS `prod_info`;
+CREATE TABLE `prod_info` (
+  `prod_id` int NOT NULL AUTO_INCREMENT,
+  `prod_title` varchar(255) DEFAULT NULL,
+  `prod_price` varchar(255) DEFAULT NULL,
+  `prod_url` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`prod_id`),
+  UNIQUE KEY `prod_id` (`prod_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE prod_info ( 
-    prod_id INTEGER AUTO_INCREMENT, 
-    prod_title VARCHAR(255), 
-    prod_price VARCHAR(255),
-    prod_url VARCHAR(255), 
-    PRIMARY KEY (prod_id)
-);
+
+DROP TABLE IF EXISTS `prod_relation`;
+CREATE TABLE `prod_relation` (
+  `prod_owner` int NOT NULL,
+  `prod` int NOT NULL,
+  KEY `prod_owner` (`prod_owner`),
+  KEY `prod` (`prod`),
+  CONSTRAINT `prod_relation_ibfk_4` FOREIGN KEY (`prod_owner`) REFERENCES `user_info` (`id`),
+  CONSTRAINT `prod_relation_ibfk_5` FOREIGN KEY (`prod`) REFERENCES `prod_info` (`prod_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
+DROP TABLE IF EXISTS `user_info`;
+CREATE TABLE `user_info` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `gender` enum('m','f','o') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+INSERT INTO `user_info` (`id`, `email`, `name`, `password`, `gender`) VALUES
+(100,	'kb529@njit.edu',	'Karan',	'',	'm');
+
+-- 2020-04-12 22:55:17
 ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'it635';
 CREATE USER 'steve'@'%' IDENTIFIED BY 'it635';
 GRANT SELECT ON scraping.prod_info TO 'steve'@'%';
